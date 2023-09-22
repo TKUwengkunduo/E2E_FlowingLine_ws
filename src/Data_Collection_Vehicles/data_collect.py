cc03c9b79cacaa30123936fbfc02a09d7655c86b
TKUwengkunduo <wengkunduo@gmail.com>
2023-09-22 11:22:31 +0800

    20230922

---
+++ src/Data_Collection_Vehicles/data_collect.py
@@ -0,0 +1,49 @@
+import time
+import cv2
+from threading import Thread
+
+
+
+class WRITE_DATA():
+    def __init__(self, proj_name='test'):
+        self.proj_name = proj_name
+        self.num = 0
+
+        time.sleep(0.5)
+        self.data_file = open('./Data/' + proj_name + '.txt', 'w')
+        self.cap = cv2.VideoCapture(0)
+        if not self.cap.isOpened():
+            print("Cannot open camera")
+            exit()
+
+    def recode(self, L_speed, R_speed):
+        file_name = self.proj_name + '_' + str(self.num)
+        print('write file : ' + file_name)
+        self.data_file.write(file_name + '_' + str(L_speed) + '_' + str(R_speed) + '\n')
+        for i in range(10):
+            self.cap.read()
+        ret, frame = self.cap.read()
+        cv2.imwrite('./Data/' + self.proj_name + '/' + file_name + '.png', frame)
+        cv2.imshow('img', frame)
+        cv2.waitKey(1)
+        self.num+=1
+
+
+    # def _main_loop(self):
+    #     while True:
+    #         ret, self.frame = self.cap.read()
+    #         cv2.imshow('img', self.frame)
+    #         cv2.waitKey(1)
+
+    # def start_main_loop_thread(self):
+    #     self.main_loop_thread = Thread(target=self._main_loop)
+    #     self.main_loop_thread.start()
+
+
+
+
+if __name__=='__main__':
+    recorder = WRITE_DATA('test', 1)
+    # recorder.start_main_loop_thread()
+
+    recorder.recode()
--- src/Data_Collection_Vehicles/main.py
+++ src/Data_Collection_Vehicles/main.py
@@ -1,5 +1,7 @@
 import motor_controller
 import communication
+import data_collect
+import time
 
 motor = motor_controller.MOTOR_2_WHEEL_MODE()
 motor.usb_initialization(usb='/dev/ttyUSB0', baudrate=1000000, protocol_version=2.0)
@@ -11,8 +13,13 @@ stratery = communication.FS_i6()
 stratery.device_initialization(usb='/dev/ttyUSB1', baudrate=115200)
 stratery.start_main_loop_thread()
 
+old_time = time.time()
+recorder = data_collect.WRITE_DATA('test')
+# recorder.start_main_loop_thread()
+
 while True:
     R_speed, L_speed = stratery.get_data()
-    # print(R_speed, ' | ', L_speed)
-
     motor.setSpeed(L_speed, R_speed)
+    if time.time()-old_time>=0.2:
+        recorder.recode(L_speed, R_speed)
+        old_time = time.time()
